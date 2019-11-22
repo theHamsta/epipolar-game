90763790974813450c22441499e7d2a8e92d43f2
Stephan Seitz <stephan.seitz@fau.de>
2019-11-22 14:21:16 +0100

    Show QMainWindow

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
-#include <iostream>
-#include <cstdlib>
+#include <QApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QException>
+#include <QFileInfo>
+#include <QMessageBox>
+#include <QtGlobal>
+#include <qt5/QtWidgets/qmainwindow.h>
 
-auto main( int argc, char** argv ) -> int
-{
-    std::cout << "Hello world!" << std::endl;
-    
-    return EXIT_SUCCESS;
+// using namespace ImageComparer;
+
+int main(int argc, char *argv[]) {
+  //     Q_INIT_RESOURCE(resources);
+#ifndef NDEBUG
+  // see http://doc.qt.io/qt-5/qtglobal.html#qSetMessagePattern for format
+  qSetMessagePattern(
+      "[%{type}] (%{time}, thread: %{threadid}) %{message} (%{file}:%{line})");
+#endif
+
+  QApplication app(argc, argv);
+
+  // QCoreApplication::setOrganizationName( "LMS" );
+  // QCoreApplication::setApplicationName( "Image Comparer" );
+  // QCoreApplication::setApplicationVersion( "0.1" );
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription(QCoreApplication::applicationName());
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.addPositionalArgument(
+      "filename", QApplication::translate("ImageComparerMain.cpp",
+                                          "First image file to open"));
+  parser.process(app);
+
+  QMainWindow mainWin;
+  mainWin.show();
+
+  return app.exec();
 }
