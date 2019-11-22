58b0437c62a9ecf18dd952dad19112f4656c4179
Stephan Seitz <stephan.seitz@fau.de>
2019-11-22 15:33:41 +0100

    Initial Structure for MainWindow

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -1,14 +1,64 @@
 #include "MainWindow.hpp"
 #include "ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+#include <GetSet/GetSet.hxx>
+#include <GetSet/GetSetIO.h>
+#include <GetSet/GetSetInternal.h>
+#include <QSettings>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+
+  readSettings();
+  this->setAcceptDrops(true);
+  GetSet<>("ini-File") = "epipolar-game.ini";
+  auto callback = [&](const GetSetInternal::Node &node) {
+    const std::string &section(node.super_section);
+    const std::string &key(node.name);
+
+    qDebug() << "[" << QString::fromStdString(section)
+             << "]:" << QString::fromStdString(key);
+
+    GetSetIO::save<GetSetIO::IniFile>(GetSet<>("ini-File"));
+  };
+  m_getSetHandler = std::make_shared<GetSetHandler>(
+      callback, GetSetInternal::Dictionary::global());
+  GetSet<bool>("View/Show FPS") = true;
+  GetSetGui::Slider("View/Slice X").setMin(0).setMax(500);
+  GetSetGui::Slider("View/Slice Y").setMin(0).setMax(200);
+  GetSetGui::Slider("View/Slice Z").setMin(0).setMax(200);
+  GetSetGui::Slider("View/Transfer Min").setMin(0).setMax(10000);
+  GetSetGui::Slider("View/Transfer Max").setMin(0).setMax(10000);
+  GetSetGui::Slider("View/Volume Range Min").setMin(-1).setMax(1);
+  GetSetGui::Slider("View/Volume Range Max").setMin(-1).setMax(1);
+  GetSetGui::Slider("View/Volume Range Iso").setMin(-1).setMax(1);
+  GetSetGui::Button("View/Set Volume Min-Max");
+  GetSetGui::Slider("View/Ray Casting Step Size").setMin(0.1).setMax(2.) = 1;
+  GetSetGui::Button("View/Show Entire Scene");
+  GetSetGui::Enum("View/Rendering Mode")
+      .setChoices("Voxel; Section2D; Section3D; EmptySkip; Trilinear; "
+                  "Tricubic; LevelSet; Volume;")
+      .setValue(6);
+  GetSetGui::File("z_Input/Input File") = "";
+  GetSetGui::Slider("z_Segmentation/Threshold").setMin(0).setMax(1);
+  // GetSet<bool>("z_Segmentation/Use relative threshold") = true;
+  GetSetIO::load<GetSetIO::IniFile>(GetSet<>("ini-File"));
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+auto MainWindow::readSettings() -> void {
+
+  QSettings settings("theHamsta", "epipolar-game");
+  settings.setValue("geometry", saveGeometry());
+  settings.setValue("windowState", saveState());
+}
+
+auto MainWindow::closeEvent(QCloseEvent *event) -> void {
+  QSettings settings("theHamsta", "epipolar-game");
+  settings.setValue("geometry", saveGeometry());
+  settings.setValue("windowState", saveState());
+  QMainWindow::closeEvent(event);
 }
--- source/MainWindow.hpp
+++ source/MainWindow.hpp
@@ -2,21 +2,29 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include <memory>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  explicit MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+protected:
+  virtual void closeEvent(QCloseEvent *event) override;
+  //virtual void dragEnterEvent(QDragEnterEvent *event) override {};
+  //virtual void dropEvent(QDropEvent *event) override;
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
+  std::shared_ptr<class GetSetHandler> m_getSetHandler;
+
+  auto readSettings() -> void;
 };
 
 #endif // MAINWINDOW_HPP
